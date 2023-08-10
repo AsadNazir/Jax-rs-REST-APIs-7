b7ec13b68ab6e775746b7e6f95c44b851b8275bb
Asad Nazir <Asad Nazir>
2023-08-10 20:22:41 +0500

    Logger Addded

--- src/main/java/com/example/demo/CustomerResource.java
+++ src/main/java/com/example/demo/CustomerResource.java
@@ -3,22 +3,39 @@ package com.example.demo;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
 
 @Path("/CustomerResource")
 public class CustomerResource {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerResource.class);
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/insert")
     public Response insert (@QueryParam("name") String name, @QueryParam("age") int age, @QueryParam("amount")double amount, @QueryParam("city") String city, @QueryParam("gender") String gender) {
 
+        //Logger
+        logger.info(new Date().toString() + "_POST_REQUEST-RECIEVED_InsertRecord");
+
+
         Service S = new Service();
         boolean res = S.insertRecord(new Customer(name,age,city,gender,amount));
 
-//        200 OK
-        if (res) return Response.status(Response.Status.OK).build();
+        //200 OK
+        if (res) {
+
+            logger.info(new Date().toString() + "_POST_Success_RecordInserted");
+            return Response.status(Response.Status.OK).build();
+
+        }
 
         //Error
-        return Response.status(Response.Status.CONFLICT).entity("Error").build();
+        logger.info(new Date().toString() + "_POST_Error_RecordNotInserted");
+        return   Response.status(Response.Status.CONFLICT).entity("Error").build();
+
 
     }
 
@@ -32,11 +49,19 @@ public class CustomerResource {
 
         boolean res = S.deleteRecord(id);
 
-        //Returning 200OK
-        if(res) return Response.status(Response.Status.OK).build();
+        //200 OK
+        if (res) {
 
-        //Not found
-        return  Response.status(Response.Status.NOT_FOUND).entity("Error").build();
+            logger.info(new Date().toString() + "_DELETE_Success_RecordDeleted");
+            return Response.status(Response.Status.OK).build();
+
+        }
+
+        //Error
+
+        logger.info(new Date().toString() + "_DELETE_Error_RecordNotFound");
+        return   Response.status(Response.Status.CONFLICT).entity("Error").build();
+
     }
 
     @Path("/update")
@@ -49,10 +74,18 @@ public class CustomerResource {
         boolean res =S.upadateRecord(id, new Customer(name,age,city,gender,amount));
 
         //200 OK
-        if(res) return Response.status(Response.Status.OK).build();
+        if (res) {
 
-        //Not found
-        return  Response.status(Response.Status.NOT_FOUND).entity("Error").build();
+            logger.info(new Date().toString() + "_PUT_Success_RecordUpdated");
+            return Response.status(Response.Status.OK).build();
+
+        }
+
+        //Error
+
+        logger.info(new Date().toString() + "_PUT_Error_RecordNotFound");
+        return   Response.status(Response.Status.CONFLICT).entity("Error").build();
+
     }
 
     @GET
@@ -60,6 +93,7 @@ public class CustomerResource {
 
     public String getAll()
     {
+        logger.info(new Date().toString() + "_GET_Success_RecordsFetched");
         return (String) new Service().getALlRecords();
     }
 
